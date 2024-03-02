4fa7b4d82c92c36a515cb77765c91a24bb4cc230
fluidize <mbxiao1@gmail.com>
2024-03-02 11:12:00 -0600

    nano + sick copy button

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-const mouse_position_output = document.getElementById('output');
-let mousePos = { x: undefined, y: undefined };
-
-window.addEventListener('mousemove', (event) => {
-  mousePos = { x: event.clientX, y: event.clientY };
-  document.documentElement.style.setProperty('--xpx',innerWidth/2-mousePos.x + 'px')
-  document.documentElement.style.setProperty('--ypx',innerHeight/2-mousePos.y + 'px')
-});
+function copy_nano() {
+  var copyText = document.getElementById("nano").innerHTML;
+  navigator.clipboard.writeText(copyText);
+}
