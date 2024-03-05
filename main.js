c1e9994b7c1afadcd24d783a8d2e06c1bc964d1c
fluidize <mbxiao1@gmail.com>
2024-03-05 17:36:31 -0600

    solana

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ function copy_nano() {
   var copyText = document.getElementById("nano").innerHTML;
   navigator.clipboard.writeText(copyText);
 }
+function copy_solana() {
+  var copyText = document.getElementById("solana").innerHTML;
+  navigator.clipboard.writeText(copyText);
+}
